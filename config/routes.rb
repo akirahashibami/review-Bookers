e672b90f55dcbbcc68eb7671f9ee4e4dec2bfb38
akirahashibami <hashibami2207@gmail.com>
2020-07-06 08:33:26 +0000

    [Platform] Bookers [review]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # ルートディレクトリ（URL/　の場所）を定義
+  root :to => 'homes#top'
+
+  # booksコントローラーのアクションを定義
+  resources :books
+
+  # なんかデリートメソッドは書かないかんらしい
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
